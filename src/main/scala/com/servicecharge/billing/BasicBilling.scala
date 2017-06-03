0bb49390cfe61178a18dc8180d63a2fa3459d2f1
Myilvahanan <viansolutions@gmail.com>
2017-06-03 02:16:18 +0100

    Basic billing implemented.  All test cases succeed.

--- src/main/scala/com/servicecharge/billing/BasicBilling.scala
+++ src/main/scala/com/servicecharge/billing/BasicBilling.scala
@@ -6,6 +6,15 @@ import com.servicecharge.menu.Menu
   * Created by Myilvahanan on 03/06/2017.
   */
 object BasicBilling extends Billing {
-  def calculateBill(shoppingList:List[String]): BigDecimal = {0.0}
-
+  def calculateBill(shoppingList:List[String]): BigDecimal = {
+    var returnValue:BigDecimal = 0.00
+    for(item <- shoppingList) {
+      if(Menu.items.contains(item)) {
+        returnValue += Menu.items.get(item).get.cost
+      } else {
+        throw new NoSuchElementException(item+" not found")
+      }
+    }
+    returnValue
+  }
 }
