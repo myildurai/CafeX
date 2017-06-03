57bad0ce2f9dbb5543ddbd6bbb02bcce0e3b28b0
Myilvahanan <viansolutions@gmail.com>
2017-06-03 02:45:45 +0100

    Testcases for defined service charges in place.  However, it will not succeed as billing with service charge object is not implemented yet.

---
+++ src/main/scala/com/servicecharge/billing/ServiceChargeBilling.scala
@@ -0,0 +1,8 @@
+package com.servicecharge.billing
+
+/**
+  * Created by Myilvahanan on 03/06/2017.
+  */
+object ServiceChargeBilling extends Billing {
+  def calculateBill(purchase: List[String]): BigDecimal = {0.0}
+}
--- src/test/scala/com/servicecharge/BillingSpec.scala
+++ src/test/scala/com/servicecharge/BillingSpec.scala
@@ -1,6 +1,6 @@
 package com.servicecharge
 
-import com.servicecharge.billing.BasicBilling
+import com.servicecharge.billing.{BasicBilling, ServiceChargeBilling}
 import com.servicecharge.menu.{ItemType, Menu}
 import com.servicecharge.shopping.Shopping
 import org.scalatest.{FlatSpec, Matchers}
@@ -64,4 +64,52 @@ class BillingSpec extends FlatSpec with Matchers{
     shopping.calculateBill(shoppingList2) should be (BigDecimal(8.00))
 
   }
+  "A shopping list with only drinks " should " calculate the price with no service charge correctly " in {
+    val shopping = new Shopping(ServiceChargeBilling)
+
+    val shoppingList1=List(Menu.COLA)
+    shopping.calculateBill(shoppingList1) should be (BigDecimal(0.50))
+
+    val shoppingList2=List(Menu.COLA, Menu.COFFEE)
+    shopping.calculateBill(shoppingList2) should be (BigDecimal(1.50))
+
+  }
+
+  "A shopping list with no hotfood " should " calculate the price with 10% service charge correctly " in {
+    val shopping = new Shopping(ServiceChargeBilling)
+
+    val shoppingList1=List(Menu.COLA, Menu.COFFEE, Menu.CHEESE_SANDWICH)
+    shopping.calculateBill(shoppingList1) should be (BigDecimal(3.85))
+
+    val shoppingList2=List(Menu.COLA, Menu.COFFEE, Menu.CHEESE_SANDWICH, Menu.CHEESE_SANDWICH, Menu.CHEESE_SANDWICH)
+    shopping.calculateBill(shoppingList2) should be (BigDecimal(8.25))
+  }
+
+  "A shopping list with hotfood " should " calculate the price with 20% service charge to a maximum 20 pounds correctly " in {
+    val shopping = new Shopping(ServiceChargeBilling)
+
+    /* 1 COLA, 1 COFFEE, 1 CHEESE SANDWICH, 2 STEAK SANDWICH FOR A BILL WHERE THE SERVICE TAX IS LESS THAN 20 POUNDS*/
+    val shoppingList1=List(Menu.COLA, Menu.COFFEE, Menu.CHEESE_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH)
+    shopping.calculateBill(shoppingList1) should be (BigDecimal(15.00))
+
+    /* 1 COLA, 1 COFFEE, 1 CHEESE SANDWICH, 39 STEAK SANDWICH FOR A BILL WHERE THE SERVICE TAX GOES OVER 20 POUNDS*/
+    val shoppingList2=List(Menu.COLA, Menu.COFFEE, Menu.CHEESE_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH,
+      Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH,
+      Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH,
+      Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH,
+      Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH,
+      Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH,
+      Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH,
+      Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH, Menu.STEAK_SANDWICH
+    )
+    shopping.calculateBill(shoppingList2) should be (BigDecimal(190.00))
+  }
+  "A shopping list with items not in the Menu " should " throw exception " in {
+    intercept[NoSuchElementException] {
+      val shopping = new Shopping(ServiceChargeBilling)
+      val shoppingList1=List(Menu.COLA, Menu.COFFEE, Menu.CHEESE_SANDWICH, "Chicken Masala", Menu.CHEESE_SANDWICH)
+      shopping.calculateBill(shoppingList1) should be (BigDecimal(3.85))
+    }
+  }
+
 }
